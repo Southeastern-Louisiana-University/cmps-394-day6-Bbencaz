bd262f935672c01004cd27f44c91b54175028821
Bbencaz <brayden.bencaz@selu.edu>
2026-03-03 18:51:52 -0600

    Lab2 before Sql

---
+++ Lab2/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, HTTPException, Depends, status
+from pydantic import BaseModel
+from typing import Annotated
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+class UserBase(BaseModel):
+    username: str
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
